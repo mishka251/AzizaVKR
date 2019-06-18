41b1ac552623f3cef705223795c1c4c482dfcfdd
Mikhail Belov <mbelov@os-company.ru>
2019-06-19 03:51:53 +0500

    init'

---
+++ AzizaVKR/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AzizaVKR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            finalRects = new List<myRectangle>();
+            borderRects = new List<myRectangle>();
+            rand = new Random();
+        }
+
+        List<myRectangle> finalRects;
+        List<myRectangle> borderRects;
+        Random rand;
+        List<int> boredrrsX;
+        List<int> boredresY;
+
+        void getBordersXY()
+        {
+            boredrrsX = new List<int>();
+            boredresY = new List<int>();
+            foreach (myRectangle mr in borderRects)
+            {
+                boredrrsX.Add(mr.x);
+                boredrrsX.Add(mr.x + mr.w);
+
+                boredresY.Add(mr.y);
+                boredresY.Add(mr.y + mr.h);
+            }
+
+            boredrrsX.Add(0);//x picturebox boreders
+            boredrrsX.Add(pictureBox1.Width);
+
+            boredresY.Add(0);//y picturebox borders
+            boredresY.Add(pictureBox1.Height);
+
+            boredrrsX = boredrrsX.Distinct().ToList();//delete duplicates
+            boredresY = boredresY.Distinct().ToList();
+
+            boredrrsX.Sort();//sort
+            boredresY.Sort();
+        }
+
+        Color getRandColor()
+        {
+            Color col = Color.FromArgb(
+
+                40 + rand.Next(215),
+                50 + rand.Next(205),
+                60 + rand.Next(195)
+            );
+
+            return col;
+
+        }
+
+        void getAllRectangles()
+        {
+            finalRects = new List<myRectangle>();
+            for (int i = 0; i < boredrrsX.Count - 1; i++)
+            {
+                for (int j = 0; j < boredresY.Count - 1; j++)
+                {
+                    myRectangle mr = new myRectangle();
+                    mr.x = boredrrsX[i];
+                    mr.y = boredresY[j];
+                    mr.w = boredrrsX[i + 1] - boredrrsX[i];
+                    mr.h = boredresY[j + 1] - boredresY[j];
+                    mr.color = getRandColor();
+
+
+
+
+                    if (!borderRects.Any(border => border.isPart(mr)))
+                        finalRects.Add(mr);
+                }
+            }
+        }
+
+
+        void calcRectangles()
+        {
+            bool has_changes = true;
+            double h = (double)pictureBox1.Width;
+            while (has_changes)
+            {
+                has_changes = false;
+                finalRects.Sort((mr1, mr2) => mr1.getM() > mr2.getM() ? -1 : mr1.getM() == mr2.getM() ? 0 : 1);
+
+                for (int i = 0; i < finalRects.Count - 1; i++)
+                {
+                    myRectangle mri = finalRects[i];
+
+                    Dictionary<double, List<myRectangle>> nearests = new Dictionary<double, List<myRectangle>>();
+
+                    for (int j = 0; j < finalRects.Count; j++)
+                        if (i != j && mri.isNearest(finalRects[j]))
+                        {
+                            double O = 0;
+                            double lambda = (mri.w + finalRects[j].w) / h;
+                            O = (mri.getM() + finalRects[j].getM()) / lambda;
+                            if (!nearests.ContainsKey(O))
+                                nearests.Add(O, new List<myRectangle>());
+                            nearests[O].Add(finalRects[j]);
+
+                        }
+
+                    if (nearests.Count > 0)
+                    {
+                        myRectangle near = nearests.Last().Value.First();
+
+                        finalRects[i] += near;
+                        finalRects.Remove(near);
+                        has_changes = true;
+                        i--;
+                    }
+
+
+                }
+
+            }
+
+        }
+
+
+
+
+
+        void ShowPic()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+
+
+            foreach (myRectangle mr in finalRects)
+                mr.Draw(gr);
+
+            foreach (myRectangle mr in borderRects)
+                mr.Draw(gr);
+
+
+
+            pictureBox1.Image = bmp;
+        }
+
+        void ShowPic2()
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+
+
+            foreach (myRectangle mr in finalRects)
+                mr.Draw(gr);
+
+            pictureBox1.Image = bmp;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getBordersXY();
+            getAllRectangles();
+            calcRectangles();
+            ShowPic();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            finalRects.Clear();
+            borderRects.Clear();
+            ShowPic();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            finalRects.Clear();
+            ShowPic();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        int stX, stY;
+        bool draggedRect = false;
+        myRectangle dragged = null;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            stX = e.X;
+            stY = e.Y;
+
+            foreach (var borRect in borderRects)
+            {
+                borRect.selected = false;
+            }
+
+
+            draggedRect = borderRects.Any(bord => bord.IsIn(e.Location));
+            if (draggedRect)
+            {
+                dragged = borderRects.Find(bord => bord.IsIn(e.Location));
+                dragged.selected = true;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (borderRects.Any(bord => bord.IsIn(e.Location)))
+            {
+                Cursor = Cursors.SizeAll;
+            }
+            else
+            {
+                Cursor = Cursors.Default;
+            }
+
+            if (!draggedRect)
+                return;
+            dragged.Move(e.X - stX, e.Y - stY);
+            stX = e.X;
+            stY = e.Y;
+            ShowPic();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Delete)
+            {
+                if (borderRects.Any(bord => bord.selected))
+                {
+                    borderRects.Remove(borderRects.Find(bord => bord.selected));
+                }
+            }
+
+            if (e.KeyData == Keys.Up)
+            {
+                if (borderRects.Any(bord => bord.selected))
+                {
+                    borderRects.Find(bord => bord.selected).Move(0, -10);
+                }
+            }
+
+            if (e.KeyData == Keys.Down)
+            {
+                if (borderRects.Any(bord => bord.selected))
+                {
+                    borderRects.Find(bord => bord.selected).Move(0, 10);
+                }
+            }
+
+            if (e.KeyData == Keys.Left)
+            {
+                if (borderRects.Any(bord => bord.selected))
+                {
+                    borderRects.Find(bord => bord.selected).Move(-10, 0);
+                }
+            }
+
+            if (e.KeyData == Keys.Right)
+            {
+                if (borderRects.Any(bord => bord.selected))
+                {
+                    borderRects.Find(bord => bord.selected).Move(10, 0);
+                }
+            }
+
+            ShowPic();
+
+
+        }
+
+        int myRound(int num)
+        {
+            return 10 * (int)Math.Round(num / 10.0);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+   
+            if (draggedRect)
+            {
+                draggedRect = false;
+                dragged = null;
+            }
+
+            ShowPic();
+            if (Math.Abs(stX - e.X) < 20 || Math.Abs(stY - e.Y) < 10)
+                return;
+
+            myRectangle mr = new myRectangle
+            {
+                x = myRound(Math.Min(e.X, stX)),
+                y = myRound(Math.Min(e.Y, stY) ),
+                w = myRound(Math.Abs(stX - e.X)),
+                h = myRound(Math.Abs(stY - e.Y)),
+                color = Color.Black
+            };
+            borderRects.Add(mr);
+            ShowPic();
+        }
+
+    }
+}
---
+++ AzizaVKR/myRectangle.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace AzizaVKR
+{
+    class myRectangle
+    {
+        public int x;
+        public int y;
+        public int w;
+        public int h;
+
+        public Color color;
+        public static Color selectColor = Color.Violet;
+        public static Color borderColor = Color.Black;
+        public bool selected;
+
+        public void Move(int dx, int dy)
+        {
+            x += dx;
+            y += dy;
+        }
+
+        public bool IsIn(Point p)
+        {
+            return
+                ((x <= p.X) && (p.X <= x + w)) &&
+                ((y <= p.Y) && (p.Y <= y + h));
+        }
+
+        public void Draw(Graphics gr)
+        {
+            gr.FillRectangle(new SolidBrush(color), x, y, w, h);
+            if (selected)
+                gr.DrawRectangle(new Pen(selectColor, 2), x -2, y - 2, w + 2, h + 2);
+            else
+                gr.DrawRectangle(new Pen(borderColor, 2), x, y , w , h );
+        }
+
+        public bool isNearest(myRectangle mr2)
+        {
+
+            if (this.x == mr2.x && this.w == mr2.w)
+            {
+                if (this.y == mr2.y + mr2.h || this.y + this.h == mr2.y)
+                    return true;
+                else
+                    return false;
+            }
+
+            if (this.y == mr2.y && this.h == mr2.h)
+            {
+                if (this.x == mr2.x + mr2.w || this.x + this.w == mr2.x)
+                    return true;
+                else
+                    return false;
+            }
+            return false;
+
+        }
+        //public static bool operator ==(myRectangle mr1, myRectangle mr2)
+        //{
+        //    return
+        //        Math.Abs(mr1.x - mr2.x) < 2 &&
+        //        Math.Abs(mr1.y - mr2.y) < 2 &&
+        //        Math.Abs(mr1.w - mr2.w) < 2 &&
+        //        Math.Abs(mr1.h - mr2.h) < 2;
+        //}
+
+        //public static bool operator !=(myRectangle mr1, myRectangle mr2)
+        //{
+        //    return !(
+        //        Math.Abs(mr1.x - mr2.x) < 2 &&
+        //        Math.Abs(mr1.y - mr2.y) < 2 &&
+        //        Math.Abs(mr1.w - mr2.w) < 2 &&
+        //        Math.Abs(mr1.h - mr2.h) < 2);
+        //}
+
+        public static myRectangle operator +(myRectangle mr1, myRectangle mr2)
+        {
+            if (!mr1.isNearest(mr2))
+                throw new ArgumentException("Не соседи");
+
+            if (mr1.x == mr2.x && mr1.w == mr2.w)
+            {
+                return new myRectangle()
+                {
+                    x = mr1.x,
+                    w = mr1.w,
+                    y = Math.Min(mr2.y, mr1.y),
+                    h = mr1.h + mr2.h,
+                    color = mr1.color
+                };
+            }
+
+            if (mr1.y == mr2.y && mr1.h == mr2.h)
+            {
+                return new myRectangle()
+                {
+                    x = Math.Min(mr2.x, mr1.x),
+                    w = mr1.w + mr2.w,
+                    y = mr1.y,
+                    h = mr1.h,
+                    color=mr1.color
+                };
+            }
+
+            throw new ArgumentException("Произошла хуйня");
+
+        }
+
+        public int getM()
+        {
+            return w * h;
+        }
+
+
+        public override string ToString()
+        {
+            return $"x={x} y={y} w={w} h={h}";
+        }
+
+        //public override bool Equals(object obj)
+        //{
+        //    if (!(obj is myRectangle))
+        //        return false;
+        //    myRectangle mr = (myRectangle)obj;
+
+        //    return mr == this;
+        //}
+
+        public bool isPart(myRectangle mr)
+        {
+            return mr.x >= this.x &&
+                mr.y >= this.y &&
+                mr.y + mr.h <= this.y + this.h &&
+                mr.x + mr.w <= this.x + this.w;
+        }
+
+    }
+}
